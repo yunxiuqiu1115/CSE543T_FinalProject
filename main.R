c0fa0eddf7d9fec44cd82a254fbb99c4b81aa23d
yahoochen97 <chenyehu@umich.edu>
2020-03-13 15:03:13 -0500

    stan test mcmc

--- main.R
+++ main.R
@@ -45,12 +45,52 @@ for (i in 1:counter) {
   stan_y[i,] = stan_y[i,]/sum(stan_y[i,])
 }
 
+# test data
+test_metadata = list()
+test_mu = list()
+test_sigma = list()
+test_y = list()
+test_nc = c()
+test_counter = 0
+
+# iterate over races
+for (state in states) {
+  pmu = data2016[data2016$state==state,c("posteriormean")]
+  pstd = data2016[data2016$state==state,c("posteriorstd")]
+  vote = data2016[data2016$state==state,c("vote")]
+  if(length(pmu)){
+    test_counter = test_counter + 1
+    test_metadata[[test_counter]] = c(state)
+    test_mu[[test_counter]] = pmu
+    test_sigma[[test_counter]] = pstd
+    test_y[[test_counter]] = vote / 100
+    test_nc = c(test_nc, length(vote))
+  }
+}
+
+# build stan data
+test_stan_mu = matrix(0,test_counter,4)
+test_stan_sigma = matrix(0.0001,test_counter,4)
+test_stan_y = matrix(0.0001,test_counter,4)
+
+for (i in 1:test_counter) {
+  test_stan_mu[i,1:test_nc[i]] = test_mu[[i]]
+  test_stan_sigma[i,1:test_nc[i]] = test_sigma[[i]]
+  test_stan_y[i,1:test_nc[i]] = test_y[[i]]
+  test_stan_y[i,] = test_stan_y[i,]/sum(test_stan_y[i,])
+}
+
+
 # define stan data structure
 stan_data = list(N = counter, 
                  mu = stan_mu, 
                  sigma = stan_sigma,
                  nc = nc,
-                 y = stan_y)
+                 y = stan_y,
+                 test_N = test_counter, 
+                 test_mu = test_stan_mu, 
+                 test_sigma = test_stan_sigma,
+                 test_nc = test_nc)
 
 # define stan model
 model <- stan_model("model.stan")
@@ -67,75 +107,43 @@ fit <- stan(file = "model.stan",
             )
 
 # summary(fit)
-fit_params = extract(fit)
+fit_params = as.data.frame(fit)
 
-# test data
-metadata = list()
-mu = list()
-sigma = list()
-y = list()
-nc = c()
-counter = 0
 
-# iterate over races
-for (state in states) {
-  pmu = data2016[data2016$state==state,c("posteriormean")]
-  pstd = data2016[data2016$state==state,c("posteriorstd")]
-  vote = data2016[data2016$state==state,c("vote")]
-  if(length(pmu)){
-    counter = counter + 1
-    metadata[[counter]] = c(state)
-    mu[[counter]] = pmu
-    sigma[[counter]] = pstd
-    y[[counter]] = vote / 100
-    nc = c(nc, length(vote))
-  }
-}
-
-# build stan data
-stan_mu = matrix(0,counter,4)
-stan_sigma = matrix(0.0001,counter,4)
-stan_y = matrix(0.0001,counter,4)
-
-for (i in 1:counter) {
-  stan_mu[i,1:nc[i]] = mu[[i]]
-  stan_sigma[i,1:nc[i]] = sigma[[i]]
-  stan_y[i,1:nc[i]] = y[[i]]
-  stan_y[i,] = stan_y[i,]/sum(stan_y[i,])
-}
 
 # prediction
 
-sample_posterior <- function(mus, sigmas, nc, gs=1, ds=1, fit_params=fit_params){
-  n = length(fit_params$alpha)
-  preds = matrix(0, nc, n*gs*ds)
-  count = 1
-  for(k in 1:n){
-    alpha = fit_params$alpha[i]
-    beta = fit_params$beta[i]
-    # g: monte carlo normal
-    for(g in 1:gs){
-      p = rep(0, 4)
-      for(j in 1:4){
-        if(j<=nc){
-          gamma = rnorm(1, mean = mus[j], sd = sigmas[j])
-          gamma = min(max(gamma, 0),1)
-          p[j] = alpha + beta*gamma
-        }
-        else{
-          p[j]=0.0001;
-        }
-      }
-      pred = rdirichlet(ds, p[1:nc])
-      preds[,count:(count+ds-1)] = t(pred)
-      count = count + ds
-    }
-  }
-  return(preds)
-}
+# define prediction function
+# sample_posterior <- function(mus, sigmas, nc, gs=1, ds=1, fit_params=fit_params){
+#   n = length(fit_params$alpha)
+#   preds = matrix(0, nc, n*gs*ds)
+#   count = 1
+#   for(k in 1:n){
+#     alpha = fit_params$alpha[i]
+#     beta = fit_params$beta[i]
+#     # g: monte carlo normal
+#     for(g in 1:gs){
+#       p = rep(0, 4)
+#       for(j in 1:4){
+#         if(j<=nc){
+#           gamma = rnorm(1, mean = mus[j], sd = sigmas[j])
+#           gamma = min(max(gamma, 0),1)
+#           p[j] = alpha + beta*gamma
+#         }
+#         else{
+#           p[j]=0.0001;
+#         }
+#       }
+#       pred = rdirichlet(ds, p[1:nc])
+#       preds[,count:(count+ds-1)] = t(pred)
+#       count = count + ds
+#     }
+#   }
+#   return(preds)
+# }
 
 # within 95% CI
-flags = matrix(0, counter, 4)
+flags = matrix(0, test_counter, 4)
 
 CYCLE = c()
 STATE = c()
@@ -146,17 +154,19 @@ VOTE = c()
 LOWER95 = c()
 UPPER95 = c()
 
-for(i in 1:counter) {
-  state = metadata[[i]]
+for(i in 1:test_counter) {
+  state = test_metadata[[i]]
   pmu = data2016[data2016$state==state,c("posteriormean")]
   pstd = data2016[data2016$state==state,c("posteriorstd")]
   vote = data2016[data2016$state==state,c("vote")]
   candidates = data2016[data2016$state==state,c("candidate")]
-  preds <- sample_posterior(stan_mu[i,], stan_sigma[i,], nc[i], gs=100, ds=100, fit_params=fit_params)
-  for(j in 1:nc[i]){
-    u=quantile(preds[j,],probs=c(0.975),names = FALSE)
-    l=quantile(preds[j,],probs=c(0.025),names = FALSE)
-    if (stan_y[i,j]<=u & stan_y[i,j]>=l){
+  # preds <- sample_posterior(stan_mu[i,], stan_sigma[i,], nc[i], gs=10, ds=1000, fit_params=fit_params)
+  for(j in 1:test_nc[i]){
+    tmp = paste('test_y[',i,',',j,']',sep='') 
+    preds = fit_params[[tmp]]
+    u=quantile(preds,probs=c(0.975),names = FALSE)
+    l=quantile(preds,probs=c(0.025),names = FALSE)
+    if (test_stan_y[i,j]<=u & test_stan_y[i,j]>=l){
       flags[i,j] = 1
     }
     print(state)
@@ -172,6 +182,7 @@ for(i in 1:counter) {
   }
 }
 
+# write results to csv
 result = data.frame(CYCLE,
                     STATE,
                     CANDIDATE,
--- model.stan
+++ model.stan
@@ -4,6 +4,10 @@ data {
   matrix[N, 4] sigma; // vector of prior Gaussian stds
   int<lower=0> nc[N]; // number of candidates each race
   vector<lower=0, upper=1>[4] y[N]; // vector of true votings
+  int<lower=0> test_N; // number of races
+  matrix[test_N, 4] test_mu; // vector of prior Gaussian means
+  matrix[test_N, 4] test_sigma; // vector of prior Gaussian stds
+  int<lower=0> test_nc[test_N]; // number of candidates each race
 }
 
 parameters {
@@ -43,5 +47,39 @@ model {
   for(i in 1:N){
     y[i] ~ dirichlet(p[i]);
   }
+}
+
+generated quantities {
+  vector[4] test_y[test_N]; // vector of true votings
+  vector[4] test_p[test_N]; // vector of underlying dirichlet parameters
+  vector[4] test_gamma[test_N]; // vector of underlying true support rates
+  for(i in 1:test_N){
+    for(j in 1:4){
+      test_gamma[i,j] = normal_rng(test_mu[i,j], test_sigma[i,j]);
+      if(test_gamma[i,j]<0){
+        test_gamma[i,j] = 0;
+      }
+      else if(test_gamma[i,j]>1){
+        test_gamma[i,j]=1;
+      }
+    }
+  }
+  for(i in 1:test_N){
+    for(j in 1:4){
+      if(j<=test_nc[i]){
+        // assume a simple linear relation
+        test_p[i,j] = alpha + beta*test_gamma[i,j];
+        if(test_p[i,j]<0){
+          test_p[i,j]=0;
+        }
+      }
+      else{
+        // hardcode unocurring candidates
+        test_p[i,j]=0.0001;
+      }
+    }
+  }
+  for (i in 1:test_N)
+    test_y[i] = dirichlet_rng(test_p[i]);
 }
 
