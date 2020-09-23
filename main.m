0c29f34c554ca3540154822d5183096fb76be011
yahoochen97 <chenyehu@umich.edu>
2020-09-23 11:49:24 -0500

    matlabrun

--- main.m
+++ main.m
@@ -12,6 +12,9 @@ function [varout]=main(TYPE, mode, tau)
 
     % define output
     varout = "";
+    disp(TYPE);
+    disp(mode);
+    disp(tau);
     
     % load gmpl, utilities and data
     addpath("gpml-matlab-v3.6-2015-07-07");
