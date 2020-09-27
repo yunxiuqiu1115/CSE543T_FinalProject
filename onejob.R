7b27cc93ecfcdb7b16118d004614f47da92be266
yahoochen97 <chenyehu@umich.edu>
2020-09-27 15:26:30 -0500

    2010 56 GP

--- onejob.R
+++ onejob.R
@@ -9,6 +9,8 @@ if (length(args)==0) {
 horizon = args[1]
 cv_year = args[2]
 TYPE = args[3]
+IDX = strtoi(args[4])
+
 
 if (TYPE=='GP'){
   search_size = 100
@@ -23,7 +25,7 @@ rstan_options(auto_write=TRUE)
 
 averaged_nlZs = c()
 
-for (b in 1:search_size){
+for (b in ((IDX-1)*5+1):IDX*5){
   # load the prior files
   input_file = paste('results/LOO', TYPE, '_' , cv_year, 'day', horizon, '_', b ,'.csv',sep='')
   output_file = paste('nlZs/', TYPE, '_' , cv_year, 'day', horizon, '_', b,'.csv',sep='')
